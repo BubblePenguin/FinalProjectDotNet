e394b04b560debc87ceeb9abfd27e9f1c67a5bcf
Ivan <kotov.senpai@gmail.com>
2021-03-19 14:56:45 +0200

    Fixed Expences tab on view

---
+++ FinalProjectDotNet/Infrastructure/BaseNotifyPropertyChanged.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FinalProjectDotNet.Infrastructure
+{
+    class BaseNotifyPropertyChanged : INotifyPropertyChanged
+    {
+        protected void Notify([CallerMemberName]string propertyName = "")
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+    }
+}
