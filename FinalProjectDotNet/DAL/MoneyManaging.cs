fcd91e7bcc0c72b4a689fc4aeb2cae212dccffd3
Ivan <kotov.senpai@gmail.com>
2021-03-19 15:54:47 +0200

    Added Autentification View

--- FinalProjectDotNet/DAL/MoneyManaging.cs
+++ FinalProjectDotNet/DAL/MoneyManaging.cs
@@ -4,10 +4,9 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace FinalProjectDotNet.Models
+namespace FinalProjectDotNet.DAL
 {
-    class MainApp
+    class MoneyManaging
     {
-        
     }
 }
